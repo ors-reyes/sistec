4a2f6064eb3f6701c062b85dec1dc1ac3ebf4e2a
ors-reyes <67842856+ors-reyes@users.noreply.github.com>
2020-12-23 14:50:13 -0300

    Add files via upload

---
+++ rack4/rackSlide.js
@@ -0,0 +1,99 @@
+window.addEventListener("load", function() {
+    let svgObject = document.getElementById('rack-object').contentDocument,
+        
+        btn12 = svgObject.querySelector('.mask12'),
+        slide12 = svgObject.querySelector('.mask12Slide'),
+        fnd12 = svgObject.querySelector('.pnt12FndTxt'),
+        
+        btn3 = svgObject.querySelector('.mask3'),
+        slide3 = svgObject.querySelector('.mask3Slide'),
+        fnd3 = svgObject.querySelector('.pnt3FndTxt'),
+
+        btnPro = svgObject.querySelector('.maskPro'),
+        slidePro = svgObject.querySelector('.maskProSlide'),
+        fndPro = svgObject.querySelector('.pntProFndTxt'),
+
+
+        btnPiso = svgObject.querySelector('.maskPiso'),
+        slidePiso = svgObject.querySelector('.maskPisoSlide'),
+        fndPiso = svgObject.querySelector('.pntPisoFndTxt'),
+
+        btnRiendas = svgObject.querySelector('.maskRiendas');
+        slideRiendas = svgObject.querySelector('.maskRiendasSlide'),
+        fndRiendas = svgObject.querySelector('.pntRienFndTxt'),
+    
+    btn12.addEventListener("click", () => {
+      fnd12.classList.toggle("btnActivo");
+      slide12.classList.toggle("slideRackActivo");
+
+      fnd3.classList.remove("btnActivo");
+      slide3.classList.remove("slideRackActivo");
+      fndPro.classList.remove("btnActivo");
+      slidePro.classList.remove("slideRackActivo");
+      fndPiso.classList.remove("btnActivo");
+      slidePiso.classList.remove("slideRackActivo");
+      fndRiendas.classList.remove("btnActivo");
+      slideRiendas.classList.remove("slideRackActivo");
+    })
+    
+    btn3.addEventListener("click", () => {
+      fnd3.classList.toggle("btnActivo");
+      slide3.classList.toggle("slideRackActivo");
+
+      fnd12.classList.remove("btnActivo");
+      slide12.classList.remove("slideRackActivo");
+      fndPro.classList.remove("btnActivo");
+      slidePro.classList.remove("slideRackActivo");
+      fndPiso.classList.remove("btnActivo");
+      slidePiso.classList.remove("slideRackActivo");
+      fndRiendas.classList.remove("btnActivo");
+      slideRiendas.classList.remove("slideRackActivo");
+    })
+
+    btnPro.addEventListener("click", () => {
+        fndPro.classList.toggle("btnActivo");
+        slidePro.classList.toggle("slideRackActivo");
+  
+        fnd12.classList.remove("btnActivo");
+        slide12.classList.remove("slideRackActivo");
+        fnd3.classList.remove("btnActivo");
+        slide3.classList.remove("slideRackActivo");
+        fndPiso.classList.remove("btnActivo");
+        slidePiso.classList.remove("slideRackActivo");
+        fndRiendas.classList.remove("btnActivo");
+        slideRiendas.classList.remove("slideRackActivo");
+      })
+
+      btnPiso.addEventListener("click", () => {
+        fndPiso.classList.toggle("btnActivo");
+        slidePiso.classList.toggle("slideRackActivo");
+  
+        fnd12.classList.remove("btnActivo");
+        slide12.classList.remove("slideRackActivo");
+        fnd3.classList.remove("btnActivo");
+        slide3.classList.remove("slideRackActivo");
+        fndPro.classList.remove("btnActivo");
+        slidePro.classList.remove("slideRackActivo");
+        fndRiendas.classList.remove("btnActivo");
+        slideRiendas.classList.remove("slideRackActivo");
+      })
+
+      btnRiendas.addEventListener("click", () => {
+        fndRiendas.classList.toggle("btnActivo");
+        slideRiendas.classList.toggle("slideRackActivo");
+  
+        fnd12.classList.remove("btnActivo");
+        slide12.classList.remove("slideRackActivo");
+        fnd3.classList.remove("btnActivo");
+        slide3.classList.remove("slideRackActivo");
+        fndPro.classList.remove("btnActivo");
+        slidePro.classList.remove("slideRackActivo");
+        fndPiso.classList.remove("btnActivo");
+        slidePiso.classList.remove("slideRackActivo");
+      })
+  
+  
+  
+  
+  
+    });
